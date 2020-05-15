72a8926f5a2b3631f083f3d31c40de33e9f1c9cc
getElementsByName <korywh@gmail.com>
2020-05-16 02:59:14 +0900

    animation

--- src/pages/run/TargetAnimationView.tsx
+++ src/pages/run/TargetAnimationView.tsx
@@ -1,9 +1,53 @@
-import * as React from 'react'
+import * as React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx"
 
-const TargetAnimationView: React.FC = () => {
-  return (
-    null
-  )
+interface StylesArgumentType {
+  durationMs: number
 }
 
-export { TargetAnimationView }
+const useStyles = makeStyles((theme) => {
+  return {
+    container: {
+      width: '100%',
+      overflow: 'hidden',
+    },
+    item: {
+      display: 'inline-block'
+    },
+    animatedItem: {
+      position: "relative",
+      "animation-name":`$horizontal-move`,
+      "animation-duration":  (props: StylesArgumentType) =>  `${props.durationMs}ms`,
+      "animation-timing-function": `${theme.transitions.easing.easeInOut}`,
+      "animation-iteration-count": "infinite",
+    },
+    // TODO: left-end ---- right-end (not overflow)
+    "@keyframes horizontal-move": {
+      "0%": {
+        left: "0",
+      },
+      "50%": {
+        left: "100%",
+      },
+      "100%": {
+        left: "0",
+      },
+    },
+  }
+});
+
+const styleArgument: StylesArgumentType = {
+  durationMs: 2000
+}
+const TargetAnimationView: React.FC = () => {
+  const classes = useStyles(styleArgument);
+
+  return (
+    <div className={classes.container}>
+      <span className={clsx(classes.animatedItem, classes.item)}>11</span>
+    </div>
+  );
+};
+
+export { TargetAnimationView };
